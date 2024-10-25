5f85cb922eeac5cbdce6a5a063fc51e911e60f4c
ngclam <ngclam1711@gmail.com>
2024-10-25 18:31:51 +0700

    Add BestSeller

--- webbanhang/src/components/Navbar.jsx
+++ webbanhang/src/components/Navbar.jsx
@@ -13,19 +13,19 @@ const Navbar = () => {
 
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
             <NavLink to='/' className='flex flex-col items-center gap-1'>
-                <p>HOME</p>
+                <p>Trang Chủ</p>
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
             </NavLink>
             <NavLink to='/collection' className='flex flex-col items-center gap-1'>
-                <p>COLLECTION</p>
+                <p>Bộ Sưu Tập</p>
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
             </NavLink>
             <NavLink to='/about' className='flex flex-col items-center gap-1'>
-                <p>ABOUT</p>
+                <p>Về Bản Thân</p>
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
             </NavLink>
             <NavLink to='/contact' className='flex flex-col items-center gap-1'>
-                <p>CONTACT</p>
+                <p>Liên Hệ</p>
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
             </NavLink>
             
@@ -55,12 +55,12 @@ const Navbar = () => {
             <div className='flex flex-col text-gray-600'>
                 <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
                     <img src={assets.dropdown_icon} className='h-4 rotate-180' alt="" />
-                    <p>Back</p>
+                    <p>Quay Về</p>
                 </div>
-                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/' >HOME</NavLink>
-                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection' >COLLECTION</NavLink>
-                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about' >ABOUT</NavLink>
-                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact' >CONTACT</NavLink>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/' >Trang Chủ</NavLink>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection' >Bộ Sưu Tập</NavLink>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about' >Về Bản Thân</NavLink>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact' >Liên Hệ</NavLink>
 
             </div>
         </div>
